55bd292e03096a04432e53dff8a2f5dab939cdb4
Dhimas <dhimas.ra48@gmail.com>
2021-07-14 17:05:55 +0700

    1

--- config.php
+++ config.php
@@ -1,6 +1,6 @@
 <?php
 
-$koneksi = mysqli_connect("localhost", "ucp", "", "toko_buku");
+$koneksi = mysqli_connect("localhost", "root", "", "toko_buku");
 
 if (mysqli_connect_errno()){
     echo "Koneksi database gagal : " . mysqli_connect_error();
